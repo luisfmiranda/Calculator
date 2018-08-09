cec198953e8fd50d98ea70195367c53efd48a920
luisfmiranda <luisfmir@gmail.com>
2018-08-09 12:37:55 -0300

    Add support for all screen sizes

--- iOS/Calculator/Calculator/CalculatorViewController.swift
+++ iOS/Calculator/Calculator/CalculatorViewController.swift
@@ -8,12 +8,42 @@ class CalculatorViewController: UIViewController {
             return Double(display.text!)!
         }
         set {
-            display.text = newValue.cleanValue
+            display.attributedText = configureText(newValue.cleanValue, for: "display")
         }
     }
     
+    @IBOutlet weak var operationDescriptionLabel: UILabel!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
     private var calculator = Calculator()
     private var userIsTyping = false
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        display.attributedText = configureText("0", for: "display")
+        operationDescriptionLabel.attributedText = configureText("0", for: "operationDescription")
+        
+        for button in buttons {
+            let oldTitle = button.title(for: UIControl.State.normal)!
+            let newTitle = configureText(oldTitle, for: "button")
+            button.setAttributedTitle(newTitle, for: UIControl.State.normal)
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        display.attributedText = configureText((display.attributedText?.string)!, for: "display")
+        operationDescriptionLabel.attributedText = configureText("0", for: "operationDescription")
+        
+        for button in buttons {
+            let oldTitle = button.title(for: UIControl.State.normal)!
+            let newTitle = configureText(oldTitle, for: "button")
+            button.setAttributedTitle(newTitle, for: UIControl.State.normal)
+        }
+    }
     
     @IBAction func touchDigit(_ sender: UIButton) {
         if let digit = sender.currentTitle {
@@ -21,14 +51,14 @@ class CalculatorViewController: UIViewController {
                 if digit == "." && display.text!.contains(".") { return }
                 
                 let textCurrentlyInDisplay = display.text!
-                display.text = textCurrentlyInDisplay + digit
+                display.attributedText = configureText(textCurrentlyInDisplay + digit, for: "display")
             } else {
                 if digit == "0" { return }
                 
                 if digit == "." {
-                    display.text = "0."
+                    display.attributedText = configureText("0.", for: "display")
                 } else {
-                    display.text = digit
+                    display.attributedText = configureText(digit, for: "display")
                 }
             }
             
@@ -54,8 +84,9 @@ class CalculatorViewController: UIViewController {
     @IBAction func touchEraseButton(_ sender: UIButton) {
         if display.text!.count > 1 {
             display.text!.removeLast()
+            display.attributedText = configureText(display.text!, for: "display")
         } else {
-            display.text = "0"
+            display.attributedText = configureText("0", for: "display")
         }
         
         if display.text! == "0" {
@@ -65,9 +96,53 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func touchClearButton(_ sender: UIButton) {
         calculator = Calculator()
-        display.text = "0"
+        display.attributedText = configureText("0", for: "display")
         userIsTyping = false
     }
+    
+    private func configureText(_ string: String, for labelType: String) -> NSAttributedString {
+        let fontSize = SizeRatios.fontSizeOffset + view.bounds.height * SizeRatios.fonts[labelType]!
+        
+        let fontWeight: UIFont.Weight
+        if labelType != "button" {
+            fontWeight = .thin
+        } else if string != "." {
+            fontWeight = .light
+        } else {
+            fontWeight = .regular
+        }
+        
+        let font = UIFont.systemFont(ofSize: fontSize, weight: fontWeight)
+        let paragraphStyle = NSMutableParagraphStyle()
+        let foregroundColor: UIColor
+        
+        if labelType == "display" || labelType == "operationDescription" {
+            paragraphStyle.alignment = .right
+            foregroundColor = UIColor.white
+        } else {
+            paragraphStyle.alignment = .center
+            
+            if Array(0...9).map( { String($0) }).contains(string) || string == "." {
+                foregroundColor = UIColor.white
+            } else if ["C", "⤺", "⌫"].contains(string) {
+                foregroundColor = #colorLiteral(red: 0.8588235294, green: 0.8588235294, blue: 0.5529411765, alpha: 1)
+            } else {
+                foregroundColor = #colorLiteral(red: 0.3803921569, green: 0.6156862745, blue: 0.9254901961, alpha: 1)
+            }
+        }
+        
+        return NSAttributedString(string: string, attributes: [.font: font, .paragraphStyle: paragraphStyle,
+                                                               .foregroundColor: foregroundColor])
+    }
+}
+
+private struct SizeRatios {
+    static let fontSizeOffset: CGFloat = 6
+    static let fonts: [String: CGFloat] = [
+        "display": 0.08,
+        "operationDescription": 0.05,
+        "button": 0.03
+    ]
 }
 
 extension Double {
